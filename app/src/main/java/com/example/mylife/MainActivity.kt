34ff4c2e13d73fb37d031f059e8ffd5e97003641
Sai-jax <82929816+Sai-jax@users.noreply.github.com>
2021-07-08 06:14:45 +0530

    just doing the duty
    added button and text view to the code and storted connecting to the main activity but ther was no time so i deleted some code to start freash again

--- app/src/main/java/com/example/mylife/MainActivity.kt
+++ app/src/main/java/com/example/mylife/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.mylife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }private fun addNickname(view: View){
+        val editText=findViewById<EditText>(R.id.nickname_edit)
+
+      
     }
 }
